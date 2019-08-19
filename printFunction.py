3e8c26750f512d1f0ab66c2152b408f93199343e
Indira196 <54293722+Indira196@users.noreply.github.com>
2019-08-20 05:59:40 +0700

    Add files via upload

---
+++ printFunction.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+print("The itsy bitsy spider climbed up the waterspout.")
+print("Down came to the rain and the washed the spider out.")
+
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came to the rain\nand the washed the spider out.")
+
+print("My", "name", "is", sep="_", end="*")
+print("IMonty", "Python", sep="*", end="*\n")
